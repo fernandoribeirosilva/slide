c9c4af939079b2c5ad7277511db7164a99cba0d5
Fernando Ribeiro da Silva <fernando1996ribeirosilva@gmail.com>
2021-11-30 13:49:51 -0300

    navegacao prev e next finalizada

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
-import Slide from "./slide.js";
+import { SlideNav } from "./slide.js";
 
-const slide = new Slide('.slide', '.slide-wrapper');
+const slide = new SlideNav('.slide', '.slide-wrapper');
 slide.init();
 
-slide.changeSlide(3);
-slide.activePrevSlide();
+slide.addArraow('.prev', '.next');
+
+console.log(slide);
