e98e0f21a3180656ae0464bfb0617f35fc5c6fc3
Fernando Ribeiro da Silva <fernando1996ribeirosilva@gmail.com>
2021-11-30 09:34:28 -0300

    final da navegação prev e nav

--- js/slide.js
+++ js/slide.js
@@ -11,6 +11,10 @@ export default class Slide {
     }
   }
 
+  transition(active) {
+    this.slide.style.transition = active ? 'transform .3s' : '';
+  }
+
   moveSlide(disX) {
     this.dist.movePosition = disX;
     this.slide.style.transform =  `translate3d(${disX}px, 0, 0)`; 
@@ -32,6 +36,7 @@ export default class Slide {
       movetype = 'touchmove';
     }
     this.wrapper.addEventListener(movetype, this.onMove);
+    this.transition(false);
   }
 
   onMove(event) {
@@ -44,6 +49,20 @@ export default class Slide {
     const movetype = (event.type === 'mouseup') ? 'mousemove' : 'touchmove';
     this.wrapper.removeEventListener(movetype, this.onMove);
     this.dist.finalPosition = this.dist.movePosition;
+    this.changeSlideOnEnd();
+    this.transition(true);
+  }
+
+  // mude o slide ao terminar do movimento
+  changeSlideOnEnd() {
+    // se o movimento do scroll da imagem for maior que 120 muda de imagem
+    if (this.dist.movement > 120 && this.index.next !== undefined) {
+      this.activeNextSlide();
+    } else if (this.dist.movement < -120 && this.index.aterior !== undefined) {
+      this.activePrevSlide();
+    } else {
+      this.changeSlide(this.index.active);
+    }
   }
 
   addSlideEvent() {
@@ -83,7 +102,7 @@ export default class Slide {
   slidesIndexNav(index) {
     const last = this.slidesArray.length - 1;// tamanho da minha array de imagen
     this.index = {
-      aterior: index === last ?  index - 1 : undefined,
+      aterior: index ?  index - 1 : undefined,
       active: index,
       next: index === last ? undefined : index + 1,
     }
@@ -96,8 +115,22 @@ export default class Slide {
     this.dist.finalPosition = activeSlide.position;
   }
 
+  // ativa o slide anterior
+  activePrevSlide() {
+    if (this.index.aterior !== undefined) {
+      this.changeSlide(this.index.aterior);
+    }
+  }
+
+  activeNextSlide() {
+    if (this.index.next !== undefined) {
+      this.changeSlide(this.index.next);
+    }
+  }
+
   init() {
     this.bindEvents();
+    this.transition(true);
     this.addSlideEvent();
     this.slidesConfig();
     return this;
